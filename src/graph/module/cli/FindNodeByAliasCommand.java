25751400e952357276a4e777bc5dec36fa63149b
Effervex <sam.sarjant@gmail.com>
2014-02-10 17:13:03 +1300

    Minor change to findnodes (refactored a method for inheritance purposes).

--- src/graph/module/cli/FindNodeByAliasCommand.java
+++ src/graph/module/cli/FindNodeByAliasCommand.java
@@ -32,14 +32,19 @@ import util.UtilityMethods;
  * @author Sam Sarjant
  */
 public class FindNodeByAliasCommand extends CollectionCommand {
+	private static final int ALIASED_OBJECT = 2;
+	private static final int DAG_NODE = 1;
+
 	@Override
 	public String helpText() {
-		return "{0} alias [caseSensitive] [exactString] : "
+		return "{0} alias [caseSensitive] [exactString] [showMatch] : "
 				+ "Return all nodes with a matching alias (optionally quoted), with "
-				+ "optional parameters [caseSensitive] (T/F) and "
-				+ "[exactString] (T/F) to perform case-sensitive "
-				+ "search or exact/prefix string search "
-				+ "(defaults true for each).";
+				+ "optional parameters [caseSensitive] (T/F), "
+				+ "[exactString] (T/F), and [showMatch] (T/F) "
+				+ "to perform case-sensitive "
+				+ "search (default true), exact/prefix string "
+				+ "searching (default true), or show the matching "
+				+ "alias (default false).";
 	}
 
 	@Override
@@ -47,6 +52,7 @@ public class FindNodeByAliasCommand extends CollectionCommand {
 		return "Finds all nodes by their name/alias.";
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void executeImpl() {
 		super.executeImpl();
@@ -57,12 +63,40 @@ public class FindNodeByAliasCommand extends CollectionCommand {
 			return;
 		}
 
+		Collection<Object> nodes = findNodes(dagHandler);
+		if (nodes == null)
+			return;
+
+		nodes = dagHandler.sort(nodes, rangeStart_, rangeEnd_);
+
+		print(nodes.size() + "|");
+		int objectIndicator = 0;
+		for (Object n : nodes) {
+			if (objectIndicator == 0) {
+				if (n instanceof DAGNode)
+					objectIndicator = DAG_NODE;
+				if (n instanceof AliasedObject)
+					objectIndicator = ALIASED_OBJECT;
+			}
+
+			if (objectIndicator == DAG_NODE)
+				print(dagHandler.textIDObject((DAGNode) n) + "|");
+			else if (objectIndicator == ALIASED_OBJECT) {
+				AliasedObject<Character, DAGNode> ao = (AliasedObject<Character, DAGNode>) n;
+				print(dagHandler.textIDObject(ao.object_) + ",\""
+						+ new String(ArrayUtils.toPrimitive(ao.alias_)) + "\"|");
+			}
+		}
+		print("\n");
+	}
+
+	protected Collection<Object> findNodes(DAGPortHandler dagHandler) {
 		Collection<AliasModule> aliasModules = null;
 		try {
 			aliasModules = getAllAliasModules(dagHandler);
 		} catch (ModuleException me) {
 			print(me.getMessage() + "\n");
-			return;
+			return null;
 		}
 
 		ArrayList<String> split = UtilityMethods.split(data, ' ');
@@ -79,31 +113,20 @@ public class FindNodeByAliasCommand extends CollectionCommand {
 		if (split.size() >= 4 && split.get(3).equals("T"))
 			aliasText = true;
 
+		Collection<Object> nodes = new TreeSet<>();
 		if (!aliasText) {
-			Collection<DAGNode> nodes = new TreeSet<>();
 			for (AliasModule aliasModule : aliasModules) {
 				nodes.addAll(aliasModule.findNodes(alias, caseSensitive,
 						exactString));
 			}
-			nodes = dagHandler.sort(nodes, rangeStart_, rangeEnd_);
 
-			print(nodes.size() + "|");
-			for (DAGNode n : nodes)
-				print(dagHandler.textIDObject(n) + "|");
 		} else {
-			Collection<AliasedObject<Character, DAGNode>> nodes = new TreeSet<>();
 			for (AliasModule aliasModule : aliasModules) {
 				nodes.addAll(aliasModule.findAliasedNodes(alias, caseSensitive,
 						exactString));
 			}
-			nodes = dagHandler.sort(nodes, rangeStart_, rangeEnd_);
-
-			print(nodes.size() + "|");
-			for (AliasedObject<Character, DAGNode> n : nodes)
-				print(dagHandler.textIDObject(n.object_) + ",\""
-						+ new String(ArrayUtils.toPrimitive(n.alias_)) + "\"|");
 		}
-		print("\n");
+		return nodes;
 	}
 
 	protected Collection<AliasModule> getAllAliasModules(
